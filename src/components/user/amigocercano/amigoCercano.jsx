322c5305b4a4241aa94b8e5b45ad9dbe409d3bc2
adrii0113 <adrian190401@gmail.com>
2022-05-31 00:29:37 +0200

    a

--- src/components/user/amigocercano/amigoCercano.jsx
+++ src/components/user/amigocercano/amigoCercano.jsx
@@ -5,7 +5,7 @@ export default function CloseFriend({user}) {
   return (
     <Link to={`/profile/${user.nombreUsuario}`} style={{ textDecoration: 'none' , color:'black'}}>
     <li className="sidebarFriend">
-      <img className="sidebarFriendImg" src={user.imagenPerfil || PF+"/person/noAvatar.png"} alt="" />
+      <img className="sidebarFriendImg" src={user.imagenPerfil || "https://res.cloudinary.com/dtmuzq8to/image/upload/v1653948899/upload_/noAvatar_qbv5yk.png"} alt="" />
       <span className="sidebarFriendName">{user.nombreUsuario}</span>
     </li>
     </Link>
